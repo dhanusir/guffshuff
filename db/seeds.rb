84117cffbb7a032a7210df135060b2af9b781386
Cool BRG <budhram.gurung01@gmail.com>
2020-11-03 15:33:18 +0530

    Task #7: Create message resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,10 @@
 5.times do |i|
   User.create(username: "User #{i+1}", password: 'user123')
 end
+
+# Create two messages from each user
+User.all.each do |user|
+  2.times do |i|
+    user.messages.create(body: "#{user.username} : #{i + 1} message")
+  end
+end
