2fbdd316bb8fe3aa739b47af9fe3c161d10002ce
Cool BRG <budhram.gurung01@gmail.com>
2020-11-03 19:54:51 +0530

    Task #12: Action restrictions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: %i[new create]
 
   def new
-
   end
 
   def create
@@ -20,5 +20,14 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:success] = 'Logged out successfully.'
     redirect_to root_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = 'You are already logged in.'
+      redirect_to root_path
+    end
   end
 end
